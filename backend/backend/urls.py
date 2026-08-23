f7a622df494f7ceb85da3fc9b4913c99bbd984cf
fanis_safiullin <fanis.fanis777@yandex.ru>
2026-08-23 14:59:12 +0300

    Fix isort linting errors in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-
-from api import views
-
-router = routers.DefaultRouter()
-router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('api.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
